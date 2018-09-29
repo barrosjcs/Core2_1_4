465843d11e4f61331c0b70b92551d07dc261e0a3
Jefferson Barros <je_caetano@hotmail.com>
2018-09-29 15:54:18 -0300

    Migration Criação de MVC com scafolding do EF

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Fiap01.Data;
+using Fiap01.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,8 +10,45 @@ namespace Fiap01.Controllers
 {
     public class HomeController : Controller
     {
+        PerguntasContext _context;
+
+        public HomeController(PerguntasContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
+            ViewBag.Nome = "Jefferson";
+            ViewData["NomeDoAluno"] = $"Outro nome na data {DateTime.Now.ToShortDateString()}";
+
+            //var pergunta = new Pergunta()
+            //{
+            //    Id = 1,
+            //    Autor = "Jefferson",
+            //    Descricao = "Dono da porra toda"
+            //};
+
+
+            return View(_context.Perguntas.ToList());
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Pergunta model)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Perguntas.Add(model);
+                await _context.SaveChangesAsync();
+                var pergunta = model;
+            }
+
             return View();
         }
 
